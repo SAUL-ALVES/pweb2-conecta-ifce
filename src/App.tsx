177194ab8d451d44d7c17b6840ea5d40f4b62fba
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2026-03-12 21:05:59 -0300

    feat: adiciona página de Login e componentes de entrada e rótulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Footer from '@/components/shared/footer'
 import NavBar from '@/components/shared/navbar'
 import HomePage from '@/pages/HomePage'
+import LoginPage from '@/pages/LoginPage'
 
 function App() {
   return (
     <>
-      <div className='flex flex-col min-h-svh'>
+      <div className="flex flex-col min-h-svh">
         <NavBar />
-        <main className='flex-1'><HomePage/></main>
-        <Footer/>
+        <main className="flex-1 flex flex-col">
+           <HomePage />
+          <LoginPage/>
+        </main>
+        <Footer />
       </div>
     </>
   )
