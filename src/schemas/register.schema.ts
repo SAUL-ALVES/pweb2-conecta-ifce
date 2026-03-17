67889834c8ab8bf624902727ff6c2e4fe8ec156a
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2026-03-16 21:10:21 -0300

    feat(auth): atualiza schema de registro com novos campos e validacoes

--- src/schemas/register.schema.ts
+++ src/schemas/register.schema.ts
@@ -6,15 +6,25 @@ export const registerSchema = z.object({
     .trim()
     .min(2, 'O nome deve ter pelo menos 2 caracteres.'),
 
-  lastName: z.string().trim().min(2, 'Sobrenome muito curto').optional(),
+  lastName: z.string().trim().min(2, 'Sobrenome muito curto'),
+  handle: z
+    .string()
+    .trim()
+    .min(3, 'O nome de usuário deve ter pelo menos 3 caracteres.')
+    .regex(/^[a-zA-Z0-9_]+$/, 'O nome de usuário só pode conter letras, números e underscores.'),
 
   email: z
     .email('E-mail inválido')
-    .endsWith('ifce.edu.br', 'Use seu e-mail institucional').optional(),
+    .endsWith('ifce.edu.br', 'Use seu e-mail institucional'),
 
-  role: z.enum(['student', 'professor', 'technician']).optional(),
+  role: z.enum(['student', 'professor', 'technician']),
 
-  campus: z.enum(['taua', 'boa_viagem', 'fortaleza']).optional(),
+  campus: z.string().nonempty(),
+  course: z
+    .string()
+    .trim()
+    .min(3, 'O nome do curso deve ter pelo menos 3 caracteres.')
+    .optional(),
 
   password: z
     .string()
