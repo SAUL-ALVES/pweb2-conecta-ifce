7709fa825c7df8ed08c60b82872daff46c4ab065
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2026-03-18 17:56:09 -0300

    refactor(core): reorganiza provedores e limpa a inicializacao do react
    - Simplifica o main.tsx para renderizar exclusivamente o componente App.
    
    - Transfere a responsabilidade do AuthProvider e RouterProvider para o App.tsx.
    
    - Remove logica de roteamento hardcoded do ponto de entrada.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import Footer from '@/shared/components/footer'
-import NavBar from '@/shared/components/navbar'
-import { Outlet } from 'react-router'
+import { AuthProvider } from '@/features/auth/contexts/AuthContext'
+import { router } from '@/routes/router'
+import { RouterProvider } from 'react-router'
 
 function App() {
   return (
-    <>
-      <div className="flex flex-col min-h-svh">
-        <NavBar />
-        <main className="flex-1 flex flex-col">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,12 @@
+import App from '@/App'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import LoginPage from '@/pages/LoginPage.tsx'
-import HomePage from '@/pages/homepage/HomePage.tsx'
-import RegisterPage from '@/pages/RegisterPage.tsx'
-import { AuthProvider } from '@/features/auth/contexts/AuthContext.tsx'
-import FeedPage from '@/pages/FeedPage.tsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: App,
-    children: [
-      {
-        index: true,
-        Component: HomePage,
-      },
-      {
-        path: '/login',
-        Component: LoginPage,
-      },
-      {
-        path: 'register',
-        Component: RegisterPage,
-      },
-      {
-        path: 'feed',
-        Component: FeedPage,
-      }
-    ],
-  },
-])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <App />
   </StrictMode>,
 )
