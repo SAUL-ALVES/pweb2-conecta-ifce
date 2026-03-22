78dac053e76e2e6dcc3f484577307f25fa0e2f31
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2026-03-22 19:50:37 -0300

    feat(ui): adiciona menu interativo de usuario na area logada
    - Adiciona componentes base de Avatar e DropdownMenu.
    
    - Cria UserMenu para exibir as opcoes da conta do usuario logado.
    
    - Integra o menu no AppLayout.

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from '@/features/auth/contexts/AuthContext'
 import NavBar from '@/shared/components/navbar'
+import UserMenu from '@/shared/components/user-menu'
 import { Navigate, Outlet } from 'react-router'
 
 function AppLayout() {
@@ -12,7 +13,18 @@ function AppLayout() {
   return (
     <>
       <div className="flex flex-col min-h-svh">
-        <NavBar />
+        <NavBar>
+          <NavBar.Brand to="/feed" />
+
+          <NavBar.Links>
+            <NavBar.Link to="/feed" text="Feed" />
+            <NavBar.Link to="/groups" text="Grupos" />
+          </NavBar.Links>
+
+          <NavBar.Actions>
+            <UserMenu/>
+          </NavBar.Actions>
+        </NavBar>
         <main className="flex-1 flex flex-col">
           <Outlet />
         </main>
